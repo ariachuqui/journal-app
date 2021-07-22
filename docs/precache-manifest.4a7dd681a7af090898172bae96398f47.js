81ded66ca48252b5ca5d88fe2d0c9001748f33ed
Ariana Chuquiruna <ari.chuquiruna@gmail.com>
2021-07-22 20:50:49 -0300

    second commit

---
+++ docs/precache-manifest.4a7dd681a7af090898172bae96398f47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0bcdf4b07b3b42e33205b8449151ce4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1a3d557ca0952fb3e38",
+    "url": "/static/css/main.08eb10f2.chunk.css"
+  },
+  {
+    "revision": "d1ebeacf72b8c1ae7347",
+    "url": "/static/js/2.4e439e46.chunk.js"
+  },
+  {
+    "revision": "94efaf9ef8dfdcc9b58d075c8086b235",
+    "url": "/static/js/2.4e439e46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1a3d557ca0952fb3e38",
+    "url": "/static/js/main.19300e8d.chunk.js"
+  },
+  {
+    "revision": "3d4175f9165103e13d56",
+    "url": "/static/js/runtime-main.b8be2bc2.js"
+  }
+]);
